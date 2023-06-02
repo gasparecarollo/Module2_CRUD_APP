f3b8fef6c65d60700868aac54bec4e6ee71b89e6
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-06-02 11:29:18 -0400

    Added src + helpers.js + data + animals.json

---
+++ index.js
@@ -0,0 +1,12 @@
+const { readJSONFile, writeJSONFile } = require("./src/helpers")
+
+
+function run() {
+
+    console.log("Welcome to our Animals App! 🐢")
+
+}
+
+run()
+
+//npm start and node index.js
